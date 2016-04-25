cc261992bb555f1db177a121b2eebed20664b348
MarkLii <markli@bu.edu>
2016-04-25 14:53:37 -0400

    added write and read function to candyJar.cpp

--- candyJar.cpp
+++ candyJar.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 #include "candyJar.hpp"
 using namespace std;
 
@@ -8,6 +9,9 @@ using namespace std;
 
 candyJar::candyJar(int Capacity){
 capacity=Capacity;
+width=40;
+height=50;
+AllJars.push_back(candyJar(capacity));
 }
 
 candyJar::candyJar(double Width, double Height, int Capacity){
@@ -15,7 +19,9 @@ width=Width;
 height=Height;
 capacity=Capacity;
 }
-
+candyJar::candyJar(vector<candy> allcandy){
+AllCandy=allcandy;
+}
 
 
 void candyJar::addCandy(candy& Candy,const candyJar& Jar){
@@ -49,13 +55,112 @@ AllCandy.push_back(Candy);
 
 }
 
+/*
+Can you write a couple functions to save the current program state to emory.
+Basically, the information to store is this: each jar objects, which there 
+are any number of needs to be stored. Each one has the following things stored
+with it: title(string), times worked on(int), descpition(string), status(I/C/O) 
+(ICO is Incomplete/Complete/Ongoing). I think the best way to do this is two functions.
+One to read/one to write. So basically, if the read function can decode each thing in a 
+loop (jar1,jar2, jar3 in any order) then I can just add some constructors to your code. 
+For the write function, it should write a single jar to the file, overwriting it if it 
+is already stored in the file. Other than that, its coming along smoothly. 
+*/
 
 
 
+/*
+void write(candyJar& Jar){
+	ofstream myfile;
+	myfile.open("jar.txt");
+	for (vector<candy>::iterator it = Jar.AllCandy.begin();it!= Jar.AllCandy.end();++it)
+		{
+			myfile<<*it<<" ";
+		}
+	myfile.close();
+}
 
+void read(){
+	ifstream myfile;
+
+	myfile.open("jar.txt");
+
+	string candy;
+
+	vector<string> Allcandy;
+
+	while (not myfile.eof()){  // read a line
+		 myfile >> candy;
+		 Allcandy.push_back(candy);
+    }
+
+    myfile.close();
+
+    // print out the contents of the file.
+    for (auto candy: Allcandy){
+    	cout << candy << endl;
+    } 
+
+}
+*/
+void write(){
+	ofstream myfile;
+	myfile.open("jar.txt");
+
+	for (vector<candy>::iterator it = Jars.begin();it!= Jars.end();++it)
+		{
+			myfile<<*it<<" "<<"\n";
+		}
+
+	myfile.close();
+
+	}
+
+
+void read(){
+	ifstream thisfile; // note: this is an IFSTREAM, "I" stands for INPUT
+
+	string theline;          // we will store the text using these variables
+	vector<string> contents;
+    
+    //read a file one line at a time with getline()
+
+	thisfile.open("jar.txt");
+
+	while (getline(thisfile,theline))  // read a line
+		 contents.push_back(theline);  // put it in the vector
+    thisfile.close(); 
+
+    // print out the contents of the file.
+
+    for (auto line: contents){
+		title=line.substr(0, findFirstof("|")); 
+		line.erase(title.size()+1,0);
+		descpition=line.substr(0, findFirstof("|"));
+		line.erase(descpition.size()+1,0);
+		timeWorkedOn=line.substr(0, findFirstof("|")); 
+		line.erase(timeWorkedOn.size()+1,0);
+		status=line.substr(0,findFirstof("|"));
+		line.erase(status.size()+1,0)
+		ICO=line.substr(0,findFirstof("|"));
+    	}
+	}
+
+
+
+void write(){}
+
+
+void reloadJars(){
+
+
+
+}
 
 
 int main(){
+
+
 
 candy Candy1 = { .width=10, .length=20};
 
@@ -67,6 +172,8 @@ candy Candy2 = { .width=10, .length=20};
 
 Jar.addCandy(Candy2, Jar);
 
+write(Jar);
+read();
 cout<<"There is "<<Jar.AllCandy.size()<<" pieces of candy"<<endl;
 
 cout<<"They are at:";
@@ -74,7 +181,36 @@ cout<<"("<<Candy1.x<<","<<Candy1.y<<")";
 cout<<"("<<Candy2.x<<","<<Candy2.y<<")"<<endl; 
 
 
+for (vector<candy>::iterator it = Jar.AllCandy.begin();it!= Jar.AllCandy.end();++it)
+
+	{
+		cout<<*it<<endl;
+	}
+
+
+
 return 0;
 }
 
 
+/* get line from file
+
+
+string theline;           // we will store the text using these variables
+	vector<string> contents;
+    
+    //read a file one line at a time with getline()
+
+	thisfile.open("reading_a_file_by_lines.cpp");
+
+	while (getline(thisfile,theline))  // read a line
+		 contents.push_back(theline);  // put it in the vector
+    
+    thisfile.close(); 
+
+    // print out the contents of the file.
+    for (auto line: contents){
+    	cout << line << endl;
+    }
+
+*/
--- candyJar.hpp
+++ candyJar.hpp
@@ -3,13 +3,45 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <fstream>
 using namespace std;
 
 struct candy{
 double width,length;
 double x,y;
+friend ostream& operator<<(ostream& os, candy c)
+	{
+		os << c.x << c.y;
+		return os;
+
+	};
+
+friend istream& operator>>(istream& is, candy c)
+	{
+		is >> c.x >> c.y;
+		return is;   
+	};
+
+
+friend ofstream &operator<<(ofstream& b, candy c)
+	{
+		b << c.x << c.y;
+		return b;
+
+	};
+
+friend ifstream &operator>>(ifstream& a, candy c)
+	{
+		a >> c.x >> c.y;
+		return a;   
+	};
+
 };
+
+
+
+
+/////////////////////////////////////////////////
 
 
 class candyJar
@@ -17,20 +49,39 @@ class candyJar
 
 public:
 	//initializer
+	int count=1;
+	ofstream myfile;
+
 	candyJar(const double,const double,const int);
 	candyJar(const int);
+	candyJar(const vector<candy>);
 	//function for adding candy
 	void addCandy(candy&, const candyJar&);
 	//a vector that stores candy
 	vector<candy> AllCandy;
+	vector<candyJar> AllJars;
+	friend ostream& operator<<(ostream& , const candyJar&);
+    friend istream& operator>>(istream& , candyJar&);
+
+    friend ofstream& operator<<(ofstream& of, const candyJar& c){
+    	of<<c.title<<"|"<<c.description<<"|"<<c.timeWorkedOn<<"|"<<c.status<<"|"<<c.ICO; 
+    	return of;
+    }
+
+    friend ifstream& operator>>(ifstream& , candyJar&);
+
+
+
 
 private:
 	//dimension of the candy jar
 	double width, height;
 	//number of pieces of candy the jar can store
 	int capacity;
-
 };
+
+
+
 
 
 #endif
